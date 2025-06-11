852b2b3e90b51b99a487ddc25e986785a1066a50
paeyden <origamercy@gmail.com>
2025-06-11 13:53:25 +0300

    checks the calories of fruits

---
+++ nutrition.py
@@ -0,0 +1,28 @@
+def nutrition():
+    fruits = {
+        "apple" : 130,
+        "avocado" : 50,
+        "banana" : 110,
+        "cantaloupe" : 50,
+        "grapefruit" : 60,
+        "grapes" : 90,
+        "honey Dew" : 50,
+        "kiwifruit" :90,
+        "lemon": 15,
+        "lime": 20,
+        "nectarine": 60,
+        "orange" :80,
+        "peach" : 60,
+        "pineapple" : 50,
+        "plums" : 70,
+        "strawberries" : 50,
+        "sweet Cherries" : 100,
+        "tangerine" : 50,
+        "watermelon" : 80
+    }
+    
+    user_input = input("Enter a fruit: ").lower()
+    if user_input in fruits:
+        print(f"Calories: {fruits[user_input]}")
+        
+nutrition()
