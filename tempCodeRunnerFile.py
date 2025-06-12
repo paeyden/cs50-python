73f3849e09bc533dec25c31ac61c18971dfa4cac
paeyden <origamercy@gmail.com>
2025-06-12 15:38:59 +0300

    python assignment for cs50

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,27 +1,26 @@
-def taqueria():
-    menu = {
-    "Baja Taco": 4.25,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
-}
-    total = 0
-
-    while True:
-        try:
-            item = input("Item: ").strip().title()
-            if item in menu:
-                total += menu[item]
-                print(f"Total : ${total:.2f}")
-        except EOFError:
-            break
+def interpreter():
+    x, y, z = input("Enter an expression: ").split()
+    operation = y
+    num1=int(x)
+    num2 = int(z)
+    if operation == "+":
+        result=num1 + num2
+        
+    elif operation=="-":
+        result = num1 - num2 
        
-taqueria()
+    elif operation=="*":
+        result=num1 * num2 
+        
+    elif operation=="/":
+        result=num1/num2
+    else:
+        print("Enter a valid operator")
+        return
+    print(f"{result:.1f}")   
+    
+    
+interpreter()    
         
     
             
